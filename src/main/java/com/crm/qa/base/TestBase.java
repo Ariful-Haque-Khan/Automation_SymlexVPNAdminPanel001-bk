d853819b1886cc689519587a2a5bac34d53fcc6b
rumy4kolpolok <rumy.kolpolok@gmail.com>
2022-04-25 11:55:18 +0600

    new two link in the affiliator menu

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -51,7 +51,7 @@ public class TestBase {
 	public static void initialization() {
 		if (props.getProperty("browser").equals("chrome")) {
 			System.setProperty("webdriver.chrome.driver",
-					"C:\\jar_files\\chromedriver_win32\\94\\chromedriver.exe");
+					"C:\\jar_files\\chromedriver_win32\\100\\chromedriver.exe");
 			driver = new ChromeDriver(); // launch chrome browser
 		} else if (props.getProperty("browser").equals("edge")) {
 			System.setProperty("webdriver.edge.driver",
