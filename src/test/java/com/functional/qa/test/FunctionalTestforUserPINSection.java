c617e25000bf135c5ab6f9d975d473b9d37acd9b
rumy4kolpolok <rumy.kolpolok@gmail.com>
2023-08-23 17:45:39 +0600

    solve dropdown issues and add module named add jain pin...

--- src/test/java/com/functional/qa/test/FunctionalTestforUserPINSection.java
+++ src/test/java/com/functional/qa/test/FunctionalTestforUserPINSection.java
@@ -6,18 +6,15 @@ package com.functional.qa.test;
 
 import java.io.IOException;
 
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
+
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
+import com.admin.qa.function.AddPINJail;
 import com.admin.qa.function.AddPINValidity;
 import com.admin.qa.function.OnlinePINActivation;
 import com.admin.qa.function.OnlinePINMigration;
-import com.admin.qa.pages.UserPermissionSectionLinks;
 import com.crm.qa.base.TestBase;
 import com.crm.qa.pages.ContactsPage;
 import com.crm.qa.pages.HomePage;
@@ -32,6 +29,7 @@ public class FunctionalTestforUserPINSection extends TestBase{
 	AddPINValidity addPINValidity;
 	OnlinePINActivation onlinePINActivation;
 	OnlinePINMigration onlinePINMigration;
+	AddPINJail addPINJail;
 	ContactsPage conPage;
 	TestUtils testUtils;
 	
@@ -49,43 +47,62 @@ public class FunctionalTestforUserPINSection extends TestBase{
 	
 	
 	/* 01. Test add validity in any PIN!!! */
-	@Ignore
+	//@Test(priority=1)
+	//@Ignore
 	public void TestAddPINValidityForm() throws IOException, InterruptedException {
 		addPINValidity = loginPage.addPINValidityLogin(props.getProperty("username"),props.getProperty("password"));
 		
 		addPINValidity.openEmailListReport();
 		addPINValidity.typeToNumberofDays();
-		//addPINValidity.typeelementDropDownField();
+		addPINValidity.typeelementDropDownField();
 		addPINValidity.typeToPINinCSVorNewLine();
 		addPINValidity.clickOnSubmitButton();
 		
 	}
 	
 	/* 02. Test active a PIN by providing the payment Information. */
-	@Ignore
+	//@Test(priority=1)
+	//@Ignore
 	public void TestOnlinePINActivationForm() throws IOException, InterruptedException {
 		onlinePINActivation = loginPage.onlinePINActivationLogin(props.getProperty("username"),props.getProperty("password"));
 		onlinePINActivation.openOnlinePINActivationPage(); /*open the Online PIN Activation 2 Page.*/
+		//try {Thread.sleep(6000);} catch (InterruptedException ie) {}
+		//onlinePINActivation.selectPaymentGateway(); /*open the Online PIN Activation 2 Page.*/
 		onlinePINActivation.typetothePaymentTNXID();/*Type in the Payment TNX ID Input Field.*/
 		onlinePINActivation.typetothePriceUSD();/*Type in the Price USD Input Field.*/
 		onlinePINActivation.typetotheUsernameandPIN();/*Type in the Username/PIN Input Field.*/
 		onlinePINActivation.clickVerifyandActivateButton();/*Click on the Verify and Activate Button.*/
+		try {Thread.sleep(4000);} catch (InterruptedException ie) {}
 		//onlinePINActivation.clickPopUpYesButton();/*Click on the Pop Up Yes Button.*/
+		onlinePINActivation.clickPopUpNoButton();/*Click on the Pop Up No Button.*/
 	}
 	
 	/* 03. Test Online PIN Migration from Old to New. */
-	@Test(priority=1)
+	//@Test(priority=1)
+	@Ignore
 	public void TestOnlinePINMigrationForm() throws IOException, InterruptedException {
 		onlinePINMigration = loginPage.onlinePINMigrationLogin(props.getProperty("username"),props.getProperty("password"));
 		onlinePINMigration.openOnlinePINMigrationPage(); /*open the Online PIN Migration Page.*/
 		onlinePINMigration.typeToNewPINandEmail(); /*Type in the New PIN / Email Input Field.*/
-		onlinePINMigration.typeTopasswordforNewPINandEmail(); /*Type in the Password for New PIN / Email Input Field.*/
+		onlinePINMigration.typeTopasswordforNewPINandEmail(); /*Type in the Password input Field for New PIN / Email Input Field.*/
 		onlinePINMigration.typeToOldPINandEmail(); /*Type in the Old PIN / Email Input Field.*/
-		onlinePINMigration.typeTopasswordforOldPINandEmail(); /*Type in the Password for Old PIN / Email Input Field.*/
+		onlinePINMigration.typeTopasswordforOldPINandEmail(); /*Type in the Password input Field for Old PIN / Email Input Field.*/
 		onlinePINMigration.clickOnmigrationNowButton(); /*Click on the Migration Now Button.*/
 		try {Thread.sleep(7000);} catch (InterruptedException ie) {}
-		onlinePINMigration.clickOnpopUpYesButton(); /*Click on the Migration Now Button.*/
+		onlinePINMigration.clickOnpopUpYesButton(); /* click on the Yes Button in the pop up alert form. */
+		//onlinePINMigration.clickPopUpNoButton();/*Click on the Pop Up No Button.*/
+	}
+	
+	/* 03. Test Add PIN Jail Bundle!!! */
+	@Test(priority=1)
+	//@Ignore
+	public void TestAddPINJailForm() throws IOException, InterruptedException {
+		addPINJail = loginPage.addPINJailLogin(props.getProperty("username"),props.getProperty("password"));
 		
+		addPINJail.openAddPINJail();
+		addPINJail.typeToPINorUsernames();
+		addPINJail.searchandtypeelementDropDownField();
+		addPINJail.clickONSaveButton();
 	}
 	
 	/*
