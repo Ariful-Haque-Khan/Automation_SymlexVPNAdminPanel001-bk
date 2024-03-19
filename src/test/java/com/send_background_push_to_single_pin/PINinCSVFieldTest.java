d28a28120ffe8df65d358e4dc0d89d740a0c9b2a
anika mimbo <anika@kolpolok.com>
2024-03-19 16:28:23 +0600

    add new 2 fields

--- src/test/java/com/send_background_push_to_single_pin/PINinCSVFieldTest.java
+++ src/test/java/com/send_background_push_to_single_pin/PINinCSVFieldTest.java
@@ -54,7 +54,7 @@ public class PINinCSVFieldTest extends TestBase{
 	//@Ignore
 	//@Test(priority=1)
 	public void enter_empty_text_into_PIN_in_CSV_input_field() throws IOException, InterruptedException {SendBackgroundorSilentPushtoSinglePINFormCommon("");	}
-	@Test(priority=2)
+	//@Test(priority=2)
 	public void enter_a_valid_text_into_PIN_in_CSV_input_field() throws IOException, InterruptedException {SendBackgroundorSilentPushtoSinglePINFormCommon("ani1yr");	}
 	//@Test(priority=3)
 	public void enter_the_above_maximum_allowed_length_of_characters_into_PIN_in_CSV_input_field() throws IOException, InterruptedException{SendBackgroundorSilentPushtoSinglePINFormCommon("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaniy1yr");}
