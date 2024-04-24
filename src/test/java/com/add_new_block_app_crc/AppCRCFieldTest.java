b992aeaae24adea0ef5eb8639394db9cf0d2a7fc
anika mimbo <anika@kolpolok.com>
2024-04-24 11:55:12 +0600

    save master

--- src/test/java/com/add_new_block_app_crc/AppCRCFieldTest.java
+++ src/test/java/com/add_new_block_app_crc/AppCRCFieldTest.java
@@ -52,45 +52,45 @@ public class AppCRCFieldTest extends TestBase{
 	public void enter_empty_text_into_App_CRC_input_field() throws IOException, InterruptedException {addNewBlockAppCRCCommon("");	}
 	@Test(priority=2)
 	public void enter_a_valid_text_into_App_CRC_input_field() throws IOException, InterruptedException {addNewBlockAppCRCCommon("ABC123");	}
-	//@Test(priority=3)
+	@Test(priority=3)
 	public void enter_the_above_maximum_allowed_length_of_characters_into_App_CRC_input_field() throws IOException, InterruptedException{addNewBlockAppCRCCommon("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaniy1yr");}
-	//@Test(priority=4)
+	@Test(priority=4)
 	public void enter_the_below_minimum_allowed_length_of_characters_into_App_CRC_input_field() throws IOException, InterruptedException{addNewBlockAppCRCCommon("a");}
-	//@Test(priority=5)
+	@Test(priority=5)
 	public void enter_only_alphabetic_characters_into_App_CRC_input_field() throws IOException, InterruptedException{addNewBlockAppCRCCommon("abcdefgh");}
-	//@Test(priority=6)
+	@Test(priority=6)
 	public void enter_numeric_characters_into_App_CRC_input_field() throws IOException, InterruptedException{addNewBlockAppCRCCommon("123456789123456789");}
-	//@Test(priority=7)
+	@Test(priority=7)
 	public void enter_a_combination_of_alphabetic_and_numeric_characters_into_App_CRC_input_field() throws IOException, InterruptedException{addNewBlockAppCRCCommon("abc1234567abc");}
-	//@Test(priority=8)
+	@Test(priority=8)
 	public void enter_special_characters_into_App_CRC_input_field() throws IOException, InterruptedException{addNewBlockAppCRCCommon("!@#$%^&*!@#$%^&*@!@#$%^&"); }
-	//@Test(priority=9)
+	@Test(priority=9)
 	public void enter_text_with_leading_whitespaces_into_App_CRC_input_field() throws IOException, InterruptedException{addNewBlockAppCRCCommon("           ani1yr"); }
-	//@Test(priority=10)
+	@Test(priority=10)
 	public void enter_text_with_trailing_whitespaces_into_App_CRC_input_field() throws IOException, InterruptedException{addNewBlockAppCRCCommon("abc1yr                   ");}
-	//@Test(priority=11)
+	@Test(priority=11)
 	public void enter_text_in_uppercase_letters_into_App_CRC_input_field() throws IOException, InterruptedException{addNewBlockAppCRCCommon("asdfghjkzxcvbnm");}
-	//@Test(priority=12)
+	@Test(priority=12)
 	public void enter_text_in_lowercases_letters_into_App_CRC_input_field() throws IOException, InterruptedException{addNewBlockAppCRCCommon("anianiani");}
-	//@Test(priority=13)
+	@Test(priority=13)
 	public void enter_text_with_a_mix_of_uppercase_and_lowercase_letter_into_App_CRC_input_field() throws IOException, InterruptedException{addNewBlockAppCRCCommon("ABCdefasASD"); }
-	//@Test(priority=14)
+	@Test(priority=14)
 	public void enter_text_with_punctuation_marks_into_App_CRC_input_field() throws IOException, InterruptedException{addNewBlockAppCRCCommon(",./;?.,;/.,./?><:;,./;.,"); }
-	//@Test(priority=15)
+	@Test(priority=15)
 	public void enter_text_with_line_breaks_or_newlines_into_App_CRC_input_field() throws IOException, InterruptedException{addNewBlockAppCRCCommon("\\\\\"ANI1yr\\\\\\\\n\\\\\\\" +\\\\r\\\\n\\\"\\r\\n\"\r\n"
 				+ "				+ \"				+ \\\"                                  \\\\\\\"ani1yr\\\\\\\""); }
-	//@Test(priority=16)
+	@Test(priority=16)
 	public void enter_text_with_unicode_characters_into_App_CRC_input_field() throws IOException, InterruptedException{addNewBlockAppCRCCommon("こんにちは"); }
-	//@Test(priority=17)
+	@Test(priority=17)
 	public void enter_text_containing_emojis_into_App_CRC_input_field() throws IOException, InterruptedException{addNewBlockAppCRCCommon("😊, 🐱, 🍕, 🎉"); }
-	//@Test(priority=18)
+	@Test(priority=18)
 	public void enter_text_with_accented_characters_into_App_CRC_input_field() throws IOException, InterruptedException{addNewBlockAppCRCCommon("á, ç, ō,  á, ç, ō");}
 	//@Test(priority=19)
 	public void enter_default_placeholder_text_into_App_CRC_input_field() throws IOException, InterruptedException{ 
 			
 	}
 
-	//@Test(priority=21)
+	@Test(priority=21)
 	public void enter_then_clear_the_text_into_coupon_code_input_field() throws IOException, InterruptedException{
 		addNewBlockAppCRC = loginPage.addNewBlockAppCRCLogin(props.getProperty("username"),props.getProperty("password")); //login to the system
 		addNewBlockAppCRC.openAddNewBlockAppCRCForm();// open the Add New (Block App CRC) Page.
@@ -99,7 +99,7 @@ public class AppCRCFieldTest extends TestBase{
 		//addNewSignUpBlacklistDomain.clickONSubmitButton();// click on save button
 		
 	}
-	//@Test(priority=22)
+	@Test(priority=22)
 	public void enter_then_undo_the_input_action_into_coupon_code_input_field() throws IOException, InterruptedException{
 		addNewBlockAppCRC = loginPage.addNewBlockAppCRCLogin(props.getProperty("username"),props.getProperty("password")); //login to the system
 		addNewBlockAppCRC.openAddNewBlockAppCRCForm();// open the Add New (Block App CRC) Page.
