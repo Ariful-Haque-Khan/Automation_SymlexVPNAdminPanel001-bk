005784a32099966dc419634ff101b4230782c887
anika mimbo <anika@kolpolok.com>
2024-06-09 12:13:41 +0600

    ip detct

--- src/test/java/com/IP_detect/IPSelectProtocolDetect.java
+++ src/test/java/com/IP_detect/IPSelectProtocolDetect.java
@@ -46,37 +46,56 @@ public class IPSelectProtocolDetect extends TestBase{
 	//@Ignore
 	public void search_ip_list_from_admin_panel() throws IOException, InterruptedException {
     	iPListElement = loginPage.addNewIPLogin(props.getProperty("username"),props.getProperty("password")); //login to the system
-    	//driver.get("https://adminportal.symlexvpn.com/vpnadmin/index.php/ip/ipList");	//navigate to the Campaign List page.
-		//Thread.sleep(4000);
-		//iPListElement.clickONEditIPButton();//taking input from the Campaign Name
+    	driver.get("https://adminportal.symlexvpn.com/vpnadmin/index.php/ip/ipList");	//navigate to the Campaign List page.
+		Thread.sleep(4000);
+		iPListElement.clickONEditIPButton();//taking input from the Campaign Name
 		
-		//Thread.sleep(4000);
-		//iPListElement.copyDataFromIPField();
+		Thread.sleep(4000);
+		iPListElement.copyDataFromIPField();
 		
-		//driver.get("https://6"+iPListElement.copyDataFromIPField());
+		driver.get("https://6"+iPListElement.copy2DataFromIPField());
 		
-		driver.get("https://194.49.53.20:9439");
+		/*driver.get("https://194.49.53.20:9439");
+		Thread.sleep(2000);
 		
 		iPListElement.advanceClicked();
 		iPListElement.advanceClickedConti();
+		Thread.sleep(4000);
 		
 		String strUrl = driver.getCurrentUrl();
 		HttpURLConnection cn = (HttpURLConnection)new URL(strUrl).openConnection();
 	    cn.setRequestMethod("HEAD");
 	    cn.connect();
 	    int res = cn.getResponseCode();
-	    Assert.assertEquals(res, 200);
-		/*driver.get("https://adminportal.symlexvpn.com/vpnadmin/index.php/campaign/campaignList");
-		campaignModuleElement.clickOnAssignButton();
-		Thread.sleep(2000);
-		campaignModuleElement.selectResellerField();
-		Thread.sleep(1000);
-		campaignModuleElement. clickOnAssignCampaignUpdateNowButton();
-		Thread.sleep(2000);*/
+	    Assert.assertEquals(res, 200);*/
+		
 	}
 	
-    
+	//@Test(priority=1)
     public void select_payment_gateway () throws IOException, InterruptedException {
+		iPListElement = loginPage.addNewIPLogin(props.getProperty("username"),props.getProperty("password")); //login to the system
+    	driver.get("https://adminportal.symlexvpn.com/vpnadmin/index.php/ip/ipList");	//navigate to the Campaign List page.
+		Thread.sleep(4000);
+		iPListElement.advance2ClickedConti();//taking input from the Campaign Name
+		
+		Thread.sleep(4000);
+		iPListElement.copyDataFromIPField();
+		
+		driver.get("https://6"+iPListElement.copyDataFromIPField());
+		
+ 
+    	 /*String xpath = "/html/body/div[2]/div/section[2]/div[2]/div/div/div[2]/div/table/tbody/tr[2]/td[3]";
+         WebElement cellElement = driver.findElement(By.xpath(xpath));
+         String cellText = cellElement.getText();
+         
+         // Perform a search with the extracted data
+         driver.get("https://www.google.com");
+         WebElement searchBox = driver.findElement(By.name("q"));
+         searchBox.sendKeys(cellText);
+         searchBox.submit();*/
+         
+         // Optional: wait for a few seconds to see the search results
+         Thread.sleep(4000);
 		
 	}
     
